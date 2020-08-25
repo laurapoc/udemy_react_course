7de49e077aa50fef4cd0c3557e2b50a63da5da9a
laurapoc <laura.pociuniene@gmail.com>
2020-08-25 11:28:05 +0300

    first commit

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import "./App.css";
+import Person from "./Person/Person";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!!!</p>
+        <Person name="Max" age="28" />
+        <Person name="Manu" age="29" >May hobbies: Racing</Person>
+        <Person name="Max-Manu" age="32" />
+
+      </div>
+    );
+
+    // return React.createElement("div", {className: "App"}, React.createElement("h1", null, "Hi, I'm a React App!!!"));
+  }
+}
+
+export default App;

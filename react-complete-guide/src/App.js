97d84bd18fc29ca3498dae3abe4ad9bb7d9dd50d
laurapoc <laura.pociuniene@gmail.com>
2020-08-25 16:02:45 +0300

    manipulating state method

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,14 +3,37 @@ import "./App.css";
 import Person from "./Person/Person";
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: "Max", age: "28"},
+      {name: "Manu", age: "29"},
+      {name: "Stephanie", age: "26"}
+    ],
+    otherObject: "some other value"
+  }
+
+switchNameHandler = () => {
+  // console.log("was clicked");
+  // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+  this.setState({
+    persons: [
+      {name: "Maximilian", age: "28"},
+      {name: "Manu", age: "29"},
+      {name: "Stephanie", age: "27"}
+    ]
+  })
+}
+
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!!!</p>
-        <Person name="Max" age="28" />
-        <Person name="Manu" age="29" >May hobbies: Racing</Person>
-        <Person name="Max-Manu" age="32" />
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >May hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
 
       </div>
     );

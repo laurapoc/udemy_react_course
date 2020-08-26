1bd332aa722a1c1dce0269a81f040094f4b42e6f
laurapoc <laura.pociuniene@gmail.com>
2020-08-26 18:24:31 +0300

    using passing method

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,40 +5,46 @@ import Person from "./Person/Person";
 class App extends Component {
   state = {
     persons: [
-      {name: "Max", age: "28"},
-      {name: "Manu", age: "29"},
-      {name: "Stephanie", age: "26"}
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 },
+      { name: "Steve", age: 29 },
     ],
-    otherObject: "some other value"
-  }
+    otherState: "some other value",
+  };
 
-switchNameHandler = () => {
-  // console.log("was clicked");
-  // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-  this.setState({
-    persons: [
-      {name: "Maximilian", age: "28"},
-      {name: "Manu", age: "29"},
-      {name: "Stephanie", age: "27"}
-    ]
-  })
-}
-
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 27 },
+        { name: "Steve", age: 29 },
+      ],
+    });
+  };
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!!!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <p>This is really working!</p>
+        <button onClick={() => this.switchNameHandler("Maximilian!!!!!")}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} >May hobbies: Racing</Person>
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, "Max!!!")}
+        >
+          My Hobbies: Racing
+        </Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
       </div>
     );
-
-    // return React.createElement("div", {className: "App"}, React.createElement("h1", null, "Hi, I'm a React App!!!"));
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 

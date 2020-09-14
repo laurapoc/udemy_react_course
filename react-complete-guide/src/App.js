5ed0c93d84b7e05c0550f174f2b467820ab8712b
laurapoc <laura.pociuniene@gmail.com>
2020-09-14 21:44:25 +0300

    rewritten same styling with css modules

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
-import "./App.css";
+import classes from "./App.module.css";
 
 import Person from "./Person/Person";
-
-
 
 class App extends Component {
   state = {
@@ -50,20 +48,8 @@ class App extends Component {
   };
 
   render() {
-    // const style = {
-    //   backgroundColor: "green",
-    //   color: "white",
-    //   font: "inherit",
-    //   border: "2px solid blue",
-    //   padding: "8px",
-    //   cursor: "pointer",
-    //   ":hover": {
-    //     backgroundColor: "lightgreen",
-    //     color: "black"
-    //   } // this styling works with radium package
-    // };
-
     let persons = null;
+    let btnClass = '';
 
     if (this.state.showPersons) {
       persons = (
@@ -82,26 +68,22 @@ class App extends Component {
         </div>
       );
 
-      // style.backgroundColor = "red";
-      // style[":hover"] = {
-      //   backgroundColor: "salmon",
-      //   color: "black",
-      // };
+      btnClass = classes.Red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if (this.state.persons.length <= 2) {
-      classes.push("red"); //classes = ["red"]
+      assignedClasses.push(classes.red); //classes = ["red"]
     }
     if (this.state.persons.length <= 1) {
-      classes.push("bold"); // classes = ["red", "bold"]
+      assignedClasses.push(classes.bold); // classes = ["red", "bold"]
     }
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>Hi, I'm a React App</h1>
-        <p className={classes.join(" ")}>This is really working!</p>
-        <button className="button" onClick={this.togglePersonsHandler}>
+        <p className={assignedClasses.join(" ")}>This is really working!</p>
+        <button className={btnClass} onClick={this.togglePersonsHandler}>
           Toggle Persons
         </button>
         {persons}

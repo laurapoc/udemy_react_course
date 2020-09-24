fce27cc1845d4a7855c48596991d9c537bda3c55
laurapoc <laura.pociuniene@gmail.com>
2020-09-24 12:13:36 +0300

    second way to use Ref

--- react-complete-guide/src/components/Persons/Person/Person.js
+++ react-complete-guide/src/components/Persons/Person/Person.js
@@ -6,8 +6,13 @@ import classes from "./Person.module.css";
 import withClass from "../../../hoc/withClass";
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.inputElement.focus();
+    this.inputElementRef.current.focus();
   }
 
   render() {
@@ -18,7 +23,7 @@ class Person extends Component {
           I'm {this.props.name} and I am {this.props.age} years old!
         </p>
         <p key="i2">{this.props.children}</p>
-        <input key="i3" ref={(inputEl) => {this.inputElement = inputEl}} type="text" onChange={this.props.changed} value={this.props.name} />
+        <input key="i3" ref={this.inputElementRef} type="text" onChange={this.props.changed} value={this.props.name} />
       </Aux>
     );
   }
